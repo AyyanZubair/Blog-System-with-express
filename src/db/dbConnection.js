aee7c8b2b6db318dcc5307c7fe50f5bea55329ba
AyyanZubair <ayyanzubair7@gmail.com>
2024-02-25 18:23:10 +0500

    create middlewares and set routes

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -1,19 +1,11 @@
 import mongoose from "mongoose";
-<<<<<<< HEAD
-=======
-import { dbName } from "../constants.js";
->>>>>>> origin/main
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const connectDB = async () => {
     try {
-<<<<<<< HEAD
         await mongoose.connect(`${process.env.Database_URI}/expBlog`);
-=======
-        await mongoose.connect(`${process.env.Database_URI}/${dbName}`);
->>>>>>> origin/main
         console.log("MongoDB Connected !!");
     } catch (error) {
         console.error("mongoDB connection failed", error);
