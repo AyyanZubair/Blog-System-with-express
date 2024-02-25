36f166250b2cfd990821af8b8cf4195ae5920481
AyyanZubair <ayyanzubair7@gmail.com>
2024-02-25 16:21:08 +0500

    create middlewares and set routes

---
+++ src/routes/blogs.routes.js
@@ -0,0 +1,19 @@
+import { createBlog, deleteBlog, findAllBlogs, getMyBlogs, totalLikesOnBlog, updateBlog } from "../controllers/blogs.controller.js"
+import { Router } from "express"
+import { addLike, disLike } from "../controllers/likes.controller.js";
+import { addComment, deleteComment, updateComment } from "../controllers/comments.controller.js";
+const router = Router();
+
+router.route("/createBlog").post(createBlog)
+router.route("/deleteBlog").post(deleteBlog)
+router.route("/updateBlog").post(updateBlog)
+router.route("/allBlogs").get(findAllBlogs)
+router.route("/myBlogs").post(getMyBlogs)
+router.route("/totalLikesOnBlog").get(totalLikesOnBlog)
+router.route("/addLike").post(addLike)
+router.route("/disLike").post(disLike)
+router.route("/addComment").post(addComment)
+router.route("/deleteComment").post(deleteComment)
+router.route("/updateComment").post(updateComment)
+
+export default router;
